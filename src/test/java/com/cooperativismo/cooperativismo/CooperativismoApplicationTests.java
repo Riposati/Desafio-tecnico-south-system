f10544047711451f6e40cbbd0b57dc166a23d8c4
guriposa <guriposa@gmail.com>
2021-12-16 02:35:54 -0300

    inclusao mockito e testes basicos iniciais

--- src/test/java/com/cooperativismo/cooperativismo/CooperativismoApplicationTests.java
+++ src/test/java/com/cooperativismo/cooperativismo/CooperativismoApplicationTests.java
@@ -1,13 +1,46 @@
 package com.cooperativismo.cooperativismo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.CoreMatchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.cooperativismo.APICooperativismo.CooperativismoApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+	    classes = CooperativismoApplication.class,
+	    webEnvironment = WebEnvironment.RANDOM_PORT
+	)
+@AutoConfigureMockMvc //need this in Spring Boot test
 class CooperativismoApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+	
 	@Test
-	void contextLoads() {
+	public void getAllSchedule() throws Exception {
+		mockMvc.perform(get("/cooperativismo/pauta"))
+//		.andDo(print())
+        .andExpect(status().isOk());
+	}
+	
+	@Test
+	public void getOneScheduleById() throws Exception {
+		mockMvc.perform(get("/cooperativismo/pauta/1"))
+//		.andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.idPauta", is(1)));
 	}
 
 }
